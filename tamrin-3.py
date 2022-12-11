4ba36f9804652dd801331fe7cf2993dcfd305d67
zahra-h79 <h.z.a.zahra.79@gmail.com>
2022-12-11 22:43:06 +0330

    added tamrin-3.py in project

---
+++ tamrin-3.py
@@ -0,0 +1,13 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> for i in range(1,a+1):
+	print((i)*' '+(-2*i + (2*a-1))*'*')
+
+	
+ *******
+  *****
+   ***
+    *
+     
+>>> 
