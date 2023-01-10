903ff8b0de7b394e345f92d235561eace25d65f6
zahra-h79 <120331471+zahra-h79@users.noreply.github.com>
2023-01-10 23:39:10 +0330

    this is tamrin5

--- tamrin-5.py
+++ tamrin-5.py
@@ -1,3 +1,6 @@
 Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print("this is a test edit ptoject")
+>>> inp = int(input("enter your number:"))
+while not inp:
+    # your code
+    inp = int(input("enter your number:"))
